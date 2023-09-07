a78cd0255a9f23a0f7b63e40ea26707f771929c1
love-to-code108 <love.to.code108@gmail.com>
2023-09-07 14:03:16 +0530

    google login system is more or less done

--- src/pages/loginPage/loginPage.jsx
+++ src/pages/loginPage/loginPage.jsx
@@ -1,11 +1,64 @@
 import "../../index.css"
 
+// REACT RELATED INPUTS
+import { useNavigate } from "react-router-dom";
+
+// IMPORTING FIREBASE RELATED STUFF BELOW
+import { signInWithPopup, signOut } from "firebase/auth";
+
+// FROM THE CONFIG FILE
+import { auth, providerGoogle } from "../../config/firebase";
+
 export const LoginPage = () => {
+    const navigate = useNavigate();
 
 
-    return(
+
+    // TRYING TO SIGN IN USING GOOGLE HERE
+    const SignInWithGoogle = async () => {
+
+        try {
+            const googleUserData = await signInWithPopup(auth, providerGoogle);
+            // console.log(googleUserData);
+            navigate("/");
+
+
+
+        } catch (err) {
+            console.error(err);
+        }
+
+    }
+
+
+
+    // LOG OUT HERE
+    const SignOut = async () => {
+
+        try {
+            await signOut(auth);
+            // console.log(googleUserData);
+            navigate("/login");
+
+
+
+        } catch (err) {
+            console.error(err);
+        }
+
+    }
+
+
+    return (
         <div>
-            This is the login Page
+            <div onClick={SignInWithGoogle}
+                className="text-5xl cursor-pointer">Sign In With Google To Continue</div>
+
+
+            <div onClick={SignOut}
+                className="text-5xl cursor-pointer">Sign Out</div>
+
+                <div>{auth?.currentUser?.email}</div>
         </div>
     );
 }
