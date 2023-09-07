8c70d863dd333141e14c833ba7c73d43fa2461e4
love-to-code108 <love.to.code108@gmail.com>
2023-09-07 12:07:35 +0530

    the basic file structure is done and the tailwind has been initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+import { LoginPage } from "./pages/loginPage/loginPage";
+
 
 function App() {
   
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       Hello world
+      <LoginPage/>
     </div>
   );
 }
---
+++ src/pages/loginPage/loginPage.jsx
@@ -0,0 +1,11 @@
+import "../../index.css"
+
+export const LoginPage = () => {
+
+
+    return(
+        <div>
+            Ok so this is working
+        </div>
+    );
+}
